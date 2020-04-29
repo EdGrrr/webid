842b0daa9304cd1119b18366163c760b9e06c5cb
Edward Gryspeerdt <e.gryspeerdt@imperial.ac.uk>
2020-04-30 00:35:47 +0100

    Now have deleting working too!

--- static/JsCode.js
+++ static/JsCode.js
@@ -2,12 +2,47 @@
 var mousePressed = false;
 var lastX, lastY;
 var ctx;
+var deleting = false;
+var delX = -1;
+var delY = -1;
+var lineDrawing = false;
+var lineX = -1;
+var lineY = -1;
 
 function InitThis() {
     ctx = document.getElementById('myCanvas').getContext("2d");
     $('#myCanvas').mousedown(function (e) {
-        mousePressed = true;
-        Draw(e.pageX - $(this).offset().left, e.pageY - $(this).offset().top, false);
+        if (deleting) {
+            if (delX >= 0) {
+                //Delete the box
+                ctx.clearRect(delX,delY,
+                              e.pageX - $(this).offset().left - delX,
+                              e.pageY - $(this).offset().top - delY);
+                delX = -1;
+                delY = -1;
+            }
+            else {
+                delX = e.pageX - $(this).offset().left;
+                delY = e.pageY - $(this).offset().top;
+                console.log(delX);
+                console.log(delY);
+            }
+        } else if (lineDrawing) {
+            if (lineX >= 0) {
+                //Delete the box
+                lineX = -1;
+                lineY = -1;
+            }
+            else {
+                lineX = e.pageX - $(this).offset().left;
+                lineY = e.pageY - $(this).offset().top;
+                console.log(lineX);
+                console.log(lineY);
+            }
+        } else {
+            mousePressed = true;
+            Draw(e.pageX - $(this).offset().left, e.pageY - $(this).offset().top, false);
+        }
     });
 
     $('#myCanvas').mousemove(function (e) {
@@ -28,15 +63,17 @@ function InitThis() {
             mousePressed = false;
             cPush();
         }
-    });
-    drawImage();
+    });  
+
+    
+    drawImage(); //Defined on template to get mask correct
 }
 
 function Draw(x, y, isDown) {
     if (isDown) {
         ctx.beginPath();
-        //ctx.globalCompositeOperation = 'source-out';
-        ctx.strokeStyle = $('#selColor').val();
+        ctx.globalCompositeOperation = 'destination-atop';
+        ctx.strokeStyle = 'red';
         ctx.lineWidth = $('#selWidth').val();
         ctx.lineJoin = "round";
         ctx.moveTo(lastX, lastY);
@@ -79,17 +116,11 @@ function cRedo() {
 
 function saveImage() {
     var dataURL = document.getElementById('myCanvas').toDataURL();
-    //s = new FormData();
-    //s.append('image', toblob(document.getElementById('myCanvas').toDataURL()));
     console.log(dataURL);
     $.ajax({
         type: "POST",
         url: "/submit_mask",
         data: {'img64': dataURL},
-        //processData : false,
-        //contentType : "multipart/form-data", 
-        // cache: false,
-        // processData: false,
     }).done(function(o) {
         console.log('saved'); 
         // If you want the file to be visible in the browser 
@@ -99,23 +130,24 @@ function saveImage() {
     });
 }
 
-function toblob(stuff) {
-    var g, type, bi, ab, ua, b, i;
-    g = stuff.split(',');
-    if (g[0].split('png')[1])
-        type = 'png';
-    else if (g[0].split('jpeg')[1])
-        type = 'jpeg';
-    else
-        return false;
-    bi = atob(g[1]);
-    ab = new ArrayBuffer(bi.length);
-    ua = new Uint8Array(ab);
-    for (i = 0; i < bi.length; i++) {
-        ua[i] = bi.charCodeAt(i);
+
+// Shortcuts
+// d - deleteing mode (draw boxes with two clicks
+// t - toggle mask
+// l - line drawing mode
+// p - polgon drawing mode
+$(document).keypress(function (e) {
+    console.log(e.which);
+    if(e.which == 100 ) {
+        deleting = !deleting;
+        delX = -1;
+        delY = -1;
+        console.log(deleting);
+    } else if (e.which == 116 ) {
+        $('#myCanvas').toggle();
+    } else if (e.which == 108) {
+        lineDrawing = !lineDrawing;
+        lineX = -1;
+        lineY = -1;
     }
-    b = new Blob([ua], {
-        type: "image/" + type
-    });
-    return b;
-}
+});
